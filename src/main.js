646358946ddb2182f37c0d5307d81d4d2a6b4ea1
priti.kumari <priti.kumari@kelltontech.com>
2019-05-16 17:38:38 +0530

    changes

--- src/main.js
+++ src/main.js
@@ -1,15 +1,16 @@
+
 import Vue from 'vue';
+import VueCarousel from 'vue-carousel';
 import App from './App.vue';
 import router from './router';
 import store from './store';
-import VueCarousel from 'vue-carousel';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 Vue.use(VueCarousel);
 
 new Vue({
   router,
   store,
-  render: h => h(App)
-}).$mount('#app')
+  render: h => h(App),
+}).$mount('#app');
--- src/router.js
+++ src/router.js
@@ -1,8 +1,8 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import Home from './views/Home.vue'
+import Vue from 'vue';
+import Router from 'vue-router';
+import Home from './views/Home.vue';
 
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
   mode: 'history',
@@ -11,12 +11,12 @@ export default new Router({
     {
       path: '/',
       name: 'home',
-      component: Home
+      component: Home,
     },
     {
       path: '/movie/:id',
       name: 'movie',
-      component: () => import(/* webpackChunkName: "about" */ './components/movieDetails.vue')
-    }
-  ]
-})
+      component: () => import(/* webpackChunkName: "about" */ './components/movieDetails.vue'),
+    },
+  ],
+});
